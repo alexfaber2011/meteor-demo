347a51cc707828ebfdaa6d1148f456b442f3808e
Kyle <mbio.kyle@gmail.com>
2014-06-29 16:53:22 -0500

    implemented table siwtching buttons

--- client/views/admin/admin.js
+++ client/views/admin/admin.js
@@ -2,7 +2,10 @@ Template.admin.helpers({
 	comments: function() {
 		return Comments.find({});
 	},
-	settings: function() {
+	tableInit: function() {
+		Session.set('table-coll', 'comments');
+	},
+	settingsComments: function() {
 		return {
 			useFontAwesome: true,
 			fields: [ 
@@ -12,11 +15,43 @@ Template.admin.helpers({
 				{ key: '_id', label: 'Delete', tmpl: Template.adminCommentDelete } 
 			]
 		};
+	},
+	settingsSpecials: function() {
+		return {
+			useFontAwesome: true,
+			fields: [ 
+				{ key: 'name', label: 'Name'},
+				{ key: 'avilable', label: 'Date'},
+				{ key: 'description', label: 'Description'},
+				{ key: 'votes', label: 'Votes'} 
+			]
+		};
+	},
+
+	displayComments: function() {
+		return (Session.get('table-coll') === 'comments');
+	},
+	displaySpecials: function() {
+		return (Session.get('table-coll') === 'specials');
+	},
+	currentTable: function() {
+		var current = Session.get('table-coll');
+		return (current.charAt(0).toUpperCase() + current.slice(1));
 	}
 });
 
 Template.admin.events({
 	'click .delete-button': function () {
 		Comments.remove({_id: this._id});
+	},
+
+	'click .table-btn': function() {
+		var current = Session.get('table-coll');
+		if(current === 'comments')
+		{
+			Session.set('table-coll', 'specials');
+		} else {
+			Session.set('table-coll', 'comments');
+		}
 	}
 });
